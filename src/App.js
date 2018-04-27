9d227e3745448a886b5cbb44288808c44b6d3844
liuhaotian <oliverblue@126.com>
2018-04-27 16:56:19 +0800

    Revert "use redux-react"
    This reverts commit 0f9e55dc794f2173072fb94dfa3ffcc828f7c42e.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { addGun, removeGun, removeGunAsync} from './index.redux';
 
-const mapStatetoProps = (state) => {
-    return {num: state}
-}
-const actionCreator = { addGun, removeGun, removeGunAsync}
-
-@connect(
-    mapStatetoProps,
-    actionCreator
-)
-class App extends React.Component {
+export default class App extends React.Component {
     // constructor(props) {
     //     super(props);
     // }
 
     render() {
-        const { num, addGun, removeGun, removeGunAsync } = this.props;
+        const { store, addGun, removeGun, removeGunAsync } = this.props;
+        const num = store.getState();
         return (
             <div>
                 <h1>现在有机枪{num}把</h1>
-                <button onClick={addGun}>申请武器</button>
-                <button onClick={removeGun}>上交武器</button>
-                <button onClick={removeGunAsync}>过两天再上交</button>
+                <button onClick={() => store.dispatch(addGun())}>申请武器</button>
+                <button onClick={() => store.dispatch(removeGun())}>上交武器</button>
+                <button onClick={() => store.dispatch(removeGunAsync())}>过两天再上交</button>
             </div>
         )
     }
 }
-
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,45 +2,27 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-
 import App from './App';
-import { counter } from './index.redux';
+import { counter, addGun, removeGun, removeGunAsync } from './index.redux';
 
-const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : () => { };
+const reduxDevtools = window.devToolsExtension ? window.devToolsExtension() : () => {};
 const store = createStore(counter, compose(
     applyMiddleware(thunk),
     reduxDevtools
 ));
-function ErYing() {
-    return <h2>二营</h2>
-}
-function QiBingLian() {
-    return <h2>骑兵连</h2>
-}
-ReactDom.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <div>
-                <ul>
-                    <li>
-                        <Link to='/'>仓库</Link>
-                    </li>
-                    <li>
-                        <Link to='erying'>二营</Link>
-                    </li>
-                    <li>
-                        <Link to='qibinglian'>骑兵连</Link>
-                    </li>
-                </ul>
-                <Route path='/' exact component={App} />
-                <Route path='/erying' component={ErYing} />
-                <Route path='/qibinglian' component={QiBingLian} />
 
-            </div>
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
-);
+function render() {
+    ReactDom.render(
+        <App
+            store={store}
+            addGun={addGun}
+            removeGun={removeGun}
+            removeGunAsync={removeGunAsync}
+        />,
+        document.getElementById('root')
+    );
+}
 
+render();
+
+store.subscribe(render)
