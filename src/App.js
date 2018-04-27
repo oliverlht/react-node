0f9e55dc794f2173072fb94dfa3ffcc828f7c42e
liuhaotian <oliverblue@126.com>
2018-04-27 15:16:35 +0800

    use redux-react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addGun, removeGun, removeGunAsync} from './index.redux';
 
-export default class App extends React.Component {
+const mapStatetoProps = (state) => {
+    return {num: state}
+}
+const actionCreator = { addGun, removeGun, removeGunAsync}
+
+@connect(
+    mapStatetoProps,
+    actionCreator
+)
+class App extends React.Component {
     // constructor(props) {
     //     super(props);
     // }
 
     render() {
-        const { store, addGun, removeGun, removeGunAsync } = this.props;
-        const num = store.getState();
+        const { num, addGun, removeGun, removeGunAsync } = this.props;
         return (
             <div>
                 <h1>现在有机枪{num}把</h1>
-                <button onClick={() => store.dispatch(addGun())}>申请武器</button>
-                <button onClick={() => store.dispatch(removeGun())}>上交武器</button>
-                <button onClick={() => store.dispatch(removeGunAsync())}>过两天再上交</button>
+                <button onClick={addGun}>申请武器</button>
+                <button onClick={removeGun}>上交武器</button>
+                <button onClick={removeGunAsync}>过两天再上交</button>
             </div>
         )
     }
 }
+
+
+export default App;
